3f980180ce0b0b7b322a5e559ee3f55c7b325f5e
Moises Rivera <morivera2015@gmail.com>
2024-01-31 21:08:41 -0500

    compd code

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const pets = require("./data");
 // init express app
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const PORT = 8080;
 
 // GET - / - returns homepage
 app.get("/", (req, res) => {
   // serve up the public folder as static index.html file
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // hello world route
@@ -27,7 +29,6 @@ app.get("/api/v1/pets", (req, res) => {
 app.get("/api/v1/pets/owner", (req, res) => {
   // get the owner from the request
   const owner = req.query.owner;
-  res.send(`${owner}`);
 
   // find the pet in the pets array
   const pet = pets.find((pet) => pet.owner === owner);
@@ -39,12 +40,13 @@ app.get("/api/v1/pets/owner", (req, res) => {
 // get pet by name
 app.get("/api/v1/pets/:name", (req, res) => {
   // get the name from the request
-  const name = 
+  const name = req.params.name;
 
   // find the pet in the pets array
   const pet = pets.find((pet) => pet.name === name);
 
   // send the pet as a response
+  res.send(pet);
 });
 
 app.listen(PORT, () => {
